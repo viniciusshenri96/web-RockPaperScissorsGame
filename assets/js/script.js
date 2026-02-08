59d8d3bbb204f4595e741197499d5763849435ed
viniciiussantos <viniciush2015@gmail.com>
2026-02-08 13:15:31 -0300

    refactoring game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,15 @@ const containerOptionPlayer = document.querySelector(".options__player");
 const containerOptionResult = document.querySelector(".options__result");
 const optionCPU = document.querySelector(".options__option--empty");
 const drawEl = document.querySelector(".options__subtitle");
+const scoreEl = document.querySelector(".header__score");
 const containerSelectionOptionAll =
   document.querySelectorAll(".selection__option");
 
+const btnRules = document.querySelector(".btn");
+
 const arrOption = ["paper", "scissors", "rock"];
 const random = Math.trunc(Math.random() * 3);
-console.log(random);
+let score = 0;
 
 containerSelectionOptionAll.forEach(function (element) {
   element.addEventListener("click", function () {
@@ -22,9 +25,9 @@ containerSelectionOptionAll.forEach(function (element) {
     let iconSVG = element.lastElementChild.src.split("/").at(-1);
 
     const html = `
-    <div class="options__option ${element.classList.value.slice(-6)}">
-    <img class="selection__icon" src="assets/images/${iconSVG}" width="99" height="118" alt="">
-    </div>
+        <div class="options__option ${element.classList.value.slice(-6)}">
+          <img class="selection__icon" src="assets/images/${iconSVG}" width="99" height="118" alt="">
+        </div>
     `;
     containerOptionPlayer.insertAdjacentHTML("afterbegin", html);
 
@@ -37,41 +40,55 @@ containerSelectionOptionAll.forEach(function (element) {
       optionCPU.insertAdjacentHTML("afterbegin", html);
       optionCPU.classList.add("grid");
       optionCPU.style.opacity = "1";
+    }, 1000);
 
-      // DEFININDO VENCEDOR
+    ////////////////////////////////////
+    ///// LOGIC GAME
+    const resultCSS = function () {
+      containerOptionResult.classList.add("grid");
+      containerOption.style.maxWidth = "938px";
+    };
+
+    setTimeout(function () {
       const player = iconSVG.split("-")[1].split(".")[0];
       const cpu = arrOption[random];
 
       const win = function () {
-        containerOptionResult.classList.add("grid");
-        containerOption.style.maxWidth = "938px";
+        resultCSS();
+        score++;
+        scoreEl.textContent = score;
       };
 
       const draw = function () {
-        containerOptionResult.classList.toggle("grid");
-        containerOption.style.maxWidth = "938px";
+        resultCSS();
         drawEl.textContent = "Draw";
       };
 
       const lose = function () {
-        containerOptionResult.classList.add("grid");
-        containerOption.style.maxWidth = "938px";
+        resultCSS();
         drawEl.textContent = "You Lose";
       };
 
-      // regras
+      const arrPlayer = ["rock", "scissors", "paper"];
+      const arrCPU = ["scissors", "paper", "rock"];
+
+      // win
+      for (let i = 0; i < arrPlayer.length; i++)
+        if (player === arrPlayer[i] && cpu === arrCPU[i]) return win();
+
+      // draw
       if (player === cpu) return draw();
 
-      if (player.includes("rock") && cpu.includes("scissors")) return win();
-
-      if (player.includes("scissors") && cpu.includes("paper")) return win();
-
-      if (player.includes("paper") && cpu.includes("rock")) return win();
-
+      // lose
       lose();
-    }, 1000);
+    }, 2000);
   });
 });
+// if (player.includes("rock") && cpu.includes("scissors")) return win();
+
+// if (player.includes("scissors") && cpu.includes("paper")) return win();
+
+// if (player.includes("paper") && cpu.includes("rock")) return win();
 
 // selectionElAll.forEach(function (element) {
 //   element.addEventListener("click", function () {
